5307e808ac6bf8f3ed379eb5eb982a270e95a650
Alban <alban.fichet@inria.fr>
2019-09-17 01:24:54 +0200

    Update pipotron.py

--- pipotron.py
+++ pipotron.py
@@ -2,28 +2,68 @@
 
 import random
 
-actions = ['tu longes', 'tu contournes', 'tu passes', 'tu descends', 'tu montes']
-directions = [('à côté', 0), ('derrière', 1), ('devant', 1), ('à droite', 0), ('à gauche', 0)]
-determinant = [('de la', 'du'), ('la', 'le')]
-places = [('guet', 1), ('combe', 0), ('grotte', 0), ('col', 1)]
-noms = ['du pendu', 'de la glotte', 'de la soupière', 'de la vulve Saint Pierre', 'du brossard', 'de la verge', 'Sainte Marie', 'de la grosse bosse']
+class Rando:
+    def __init__(self):
+        self.actions = ['tu longes', 'tu contournes', 'tu passes', 'tu descends', 'tu montes']
+        self.directions = [('à côté', 0), ('derrière', 1), ('devant', 1), ('à droite', 0), ('à gauche', 0)]
+        self.determinant = [('de la', 'du'), ('la', 'le')]
+        self.places = [('guet', 1), ('combe', 0), ('grotte', 0), ('col', 1), ('pré', 1)]
+        self.noms = [
+            'du pendu', 
+            'de la glotte', 
+            'de la soupière', 
+            'de la vulve Saint Pierre', 
+            'du brossard', 
+            'de la verge',
+            'Sainte Marie', 
+            'de la grosse bosse',
+            'du vieux coquin',
+            'de la belette',
+            'qui tue',
+            'de la goutière',
+            'Saint Georges',
+            'des perrières',
+            'du framboisier',
+            'de la mouille',
+            'aux fleurs',
+            'de la goute'
+        ]
+        self.liaisons = ['Bon, déjà,', 'Ensuite', 'Après', 'Puis', 'Enfin']
 
-liaisons = ['Bon, déjà,', 'Ensuite', 'Après', 'Puis', 'Enfin']
+    def get_str_adpn(self):
+        a = random.randint(0, len(self.actions) - 1)
+        d = random.randint(0, len(self.directions) - 1)
+        p = random.randint(0, len(self.places) - 1)
+        n = random.randint(0, len(self.noms) - 1)
 
+        action = self.actions[a]
+        dir_place = self.get_dir_place(d, p)
+        nom = self.noms[n]
+
+        self.noms = self.noms[0:n] + self.noms[n+1:len(self.noms)+1]
+
+        return action, dir_place, nom
+
+    def get_dir_place(self, idx_dir, idx_place):
+        direct, det = self.directions[idx_dir]
+        place, genre = self.places[idx_place]
+
+        return ' '.join([direct, self.determinant[det][genre], place])
+
+rando = Rando()
 bullshit = ''
 
-for i in range(len(liaisons)):
-    a = random.randint(0, len(actions) - 1)
-    d = random.randint(0, len(directions) - 1)
-    p = random.randint(0, len(places) - 1)
-    n = random.randint(0, len(noms) - 1)
+for i in range(len(rando.liaisons)):
+    action, dir_place, nom = rando.get_str_adpn()
+    bullshit += ' '.join([rando.liaisons[i], action, dir_place, nom]) + '. '
     
-    direct, det = directions[d]
-    place, genre = places[p] 
-    
-    dir_place = ' '.join([direct, determinant[det][genre], place])
-    
-    bullshit += ' '.join([liaisons[i], actions[a], dir_place, noms[n]]) + '. '
-    noms = noms[0:n] + noms[n+1:len(noms)+1]
-    
+    r = random.randint(0, 1)
+    if r > 0:
+        action, dir_place, nom = rando.get_str_adpn()
+        inst = ' '.join([dir_place.capitalize(), nom])
+                         
+        action, dir_place, nom = rando.get_str_adpn()                         
+        bullshit += inst + ', ' + ' '.join([action, dir_place, nom]) + '. '
+
+        
 print(bullshit)
